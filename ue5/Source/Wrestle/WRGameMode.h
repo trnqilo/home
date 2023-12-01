72dc4e9a918fde5286c55da4793e799e9438a6e4
dj-kt <102042358+dj-kt@users.noreply.github.com>
2023-12-01 17:29:56 -0600

    Wed Sep 10 11:51:12 PM CDT 2025

---
+++ ue5/Source/Wrestle/WRGameMode.h
@@ -0,0 +1,12 @@
+// Source/Wrestle/WRGameMode.h
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "WRGameMode.generated.h"
+
+UCLASS()
+class WRESTLEBOXES_API AWRGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+public:
+    AWRGameMode();
+};
